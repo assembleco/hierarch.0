afe75616d62a71b67e44afb9524ab7cebd10e2bc
grace <grace.c.youngblood@gmail.com>
2020-11-30 19:57:28 -0500

    break up styles to individual components.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,57 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import logo from './logo.svg';
 
 function App() {
   return (
     <Layout>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </Link>
+      </Header>
     </Layout>
   );
 }
 
+const LogoSpin = keyframes`
+from { transform: rotate(0deg); }
+to { transform: rotate(360deg); }
+`
+
 const Layout = styled.div`
 text-align: center;
+`
 
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
+const Logo = styled.img`
+height: 40vmin;
+pointer-events: none;
 
 @media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
+  animation: ${LogoSpin} infinite 20s linear;
 }
+`
 
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
+const Header = styled.header`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+`
 
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
+const Link = styled.a`
+color: #61dafb;
 `
 
 export default App;
