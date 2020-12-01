0255a2057d820cf8bef05c89768a593116a6a801
grace <grace.c.youngblood@gmail.com>
2020-12-01 15:43:07 -0500

    change `:3001` -> `:3000`

--- hierarch/index.js
+++ hierarch/index.js
@@ -8,7 +8,7 @@ responder.use(bodyParser.json())
 
 // https://enable-cors.org/server_expressjs.html
 responder.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001")
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
 });
