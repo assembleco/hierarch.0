7830d3118856bfe4901634b58a3b6ef1d1802716
grace <grace.c.youngblood@gmail.com>
2020-11-30 19:51:38 -0500

    use `styled-components`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import styled from "styled-components"
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +18,48 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+text-align: center;
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+`
 
 export default App;
