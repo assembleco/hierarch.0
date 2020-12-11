793579022c7006b1f8d2c76d4ec22be2d830bf9f
grace <grace.c.youngblood@gmail.com>
2020-12-11 03:35:50 -0500

    add an element displaying each hierarchical block.

--- src/hierarch/hierarchy.js
+++ src/hierarch/hierarchy.js
@@ -8,11 +8,17 @@ const Hierarchy = ({ h }) => (
 
 const display_hierarchy_index = (index, hierarchy) => (
     hierarchy[2].map(h => (
-        ("  ".repeat(index)) +
-        h[3] +
-        "\n" +
-        display_hierarchy_index(index + 1, h)
-    )).join("\n")
+        <>
+        {("  ".repeat(index))}
+        <Hierarchical name={h[3]} begin={h[0]} end={h[1]} />
+        {"\n"}
+        {display_hierarchy_index(index + 1, h)}
+        </>
+    ))
+)
+
+const Hierarchical = ({name, begin, end}) => (
+    <span>{name}</span>
 )
 
 export default Hierarchy
