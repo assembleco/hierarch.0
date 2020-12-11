a59826d29961407cda7d49d8bead4bd7a1f3bd11
grace <grace.c.youngblood@gmail.com>
2020-12-11 15:15:32 -0500

    reset `src/App.js`.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <Header>
         <Logo src={logo} alt="logo" />
         <p>
-          Edit <code>src/App</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
           Learn React
