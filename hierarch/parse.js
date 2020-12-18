ff60496fe567156fc3ceb6b2a33e775501210e79
grace <grace.c.youngblood@gmail.com>
2020-12-18 16:28:19 -0500

    small change: inline `approach.query`.

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -99,28 +99,27 @@ const apply_change = (change) => {
         }
 
         var approach = {
-            query: `(jsx_element
-                open_tag: (
-                    jsx_opening_element
-                    name: (_) @opening-name
-                    attribute: (jsx_attribute (property_identifier) @_source "=" (_) @source)
-                    attribute: (jsx_attribute (property_identifier) @_code "=" (_) @code)
-                    )
-                .
-                (jsx_text) @children
-                .
-                close_tag: (jsx_closing_element name: (_) @closing-name)
-                (#eq? @_source "source")
-                (#eq? @_code "code")
-                (#eq? @opening-name "Lens.Change")
-                (#eq? @closing-name "Lens.Change")
-            ) @element`,
             change_nodes: _ => ({
                 element: (change, _) => change.upgrade,
             }),
         }
 
-        matches = program.query(approach.query)
+        matches = program.query(`(jsx_element
+            open_tag: (
+                jsx_opening_element
+                name: (_) @opening-name
+                attribute: (jsx_attribute (property_identifier) @_source "=" (_) @source)
+                attribute: (jsx_attribute (property_identifier) @_code "=" (_) @code)
+                )
+            .
+            (jsx_text) @children
+            .
+            close_tag: (jsx_closing_element name: (_) @closing-name)
+            (#eq? @_source "source")
+            (#eq? @_code "code")
+            (#eq? @opening-name "Lens.Change")
+            (#eq? @closing-name "Lens.Change")
+        ) @element`)
         matches.forEach(m => {
             // change by nodes
             var keys = Object.keys(approach.change_nodes(program))
