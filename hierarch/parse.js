31b439dc8b572e7760551628109224c716165acb
grace <grace.c.youngblood@gmail.com>
2020-12-18 16:48:26 -0500

    small change: inline same-scope function call.

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -118,9 +118,7 @@ const apply_change = (change) => {
             // change by nodes
             var captures = m.captures.filter(c => c.name === 'element')
             captures.forEach(c => {
-                var upgrade = (change) => change.upgrade
-                upgrade = upgrade(change)
-
+                var upgrade = change.upgrade
                 program.replace_by_node(c.node, upgrade, {})
             })
         })
