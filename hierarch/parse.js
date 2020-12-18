d28517690b50333b6fe6d73ebc71dd1667ac7127
grace <grace.c.youngblood@gmail.com>
2020-12-18 16:42:25 -0500

    small change: inline `forEach` on single-element array.

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -120,23 +120,21 @@ const apply_change = (change) => {
             }
 
             // change by nodes
-            var keys = ['element']
-            keys.forEach((k) => {
-                var captures = m.captures.filter(c => c.name === k)
-                captures.forEach(c => {
-                    var upgrade = changeable_nodes[k]
-                    var options = {}
+            var k = 'element'
+            var captures = m.captures.filter(c => c.name === k)
+            captures.forEach(c => {
+                var upgrade = changeable_nodes[k]
+                var options = {}
 
-                    if(upgrade instanceof Array) {
-                        options = upgrade[1]
-                        upgrade = upgrade[0]
-                    }
+                if(upgrade instanceof Array) {
+                    options = upgrade[1]
+                    upgrade = upgrade[0]
+                }
 
-                    if(typeof upgrade === "function")
-                        upgrade = upgrade(change, c)
+                if(typeof upgrade === "function")
+                    upgrade = upgrade(change, c)
 
-                    program.replace_by_node(c.node, upgrade, options)
-                })
+                program.replace_by_node(c.node, upgrade, options)
             })
         })
 
