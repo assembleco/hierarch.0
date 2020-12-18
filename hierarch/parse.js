4b40c00d1d582a23875ac358b313f3a5f50a19c3
grace <grace.c.youngblood@gmail.com>
2020-12-18 16:20:18 -0500

    small change: drop unused assignment

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -45,9 +45,6 @@ const apply_lens = (range) => {
 }
 
 const add_dependency = (program) => {
-    var approach = {
-    }
-
     matches = program.query(`
     (import_statement (import_clause (identifier) @identifier) source: (string) @source
         (#match? @source "./hierarch/lens")
