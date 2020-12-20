a2334acaede937fa9b0b20d1d0960cb1f17985ac
grace <grace.c.youngblood@gmail.com>
2020-12-20 18:11:52 -0500

    drop React link, use base hierarch repo.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
-          Learn React
+        <Link href="https://github.com/assembleapp/hierarch" target="_blank" rel="noopener noreferrer" >
+          Learn Hierarch.
         </Link>
       </Header>
     </Layout>
