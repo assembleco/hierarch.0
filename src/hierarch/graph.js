d6633fc3afaef32854ce733e82a03146c14599e1
grace <grace.c.youngblood@gmail.com>
2020-12-24 08:07:01 -0500

    add apollo in an unlinked module.

---
+++ src/hierarch/graph.js
@@ -0,0 +1,50 @@
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { WebSocketLink } from "apollo-link-ws"
+import { createHttpLink } from "apollo-link-http"
+import { getMainDefinition } from "apollo-utilities"
+import { setContext } from "apollo-link-context"
+import { split } from "apollo-link"
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${process.env.REACT_APP_URL_HASURA}`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        // TODO is this needed if used alongside authLink?
+        "x-hasura-access-key": process.env.REACT_APP_HASURA_PASSCODE,
+      }
+    }
+  },
+
+})
+
+const httpLink = createHttpLink({
+  uri: `http://${process.env.REACT_APP_URL_HASURA}`
+})
+
+const authLink = setContext((_, { headers }) => (
+  { headers: {
+    ...headers,
+      "x-hasura-access-key": process.env.REACT_APP_HASURA_PASSCODE,
+  } }
+))
+
+const link = split(({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+)
+
+const graph = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache(),
+})
+
+export default graph
