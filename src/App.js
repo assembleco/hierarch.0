8ebc8a58ceb9bdf1bd34b73ccf991e4e7a6ae8bd
grace <grace.c.youngblood@gmail.com>
2021-01-05 22:08:35 -0500

    Rearrange homepage; display as a single column.

--- src/App.js
+++ src/App.js
@@ -5,53 +5,53 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <Box original={Layout} code="abc1">
-      <Box original={Header} code="abc2">
-        <Box original={Logo} code="abc31" src={logo} alt="logo" />
-
-        <Box original={P} code="abc4">
-          Change <Box original={Code} code="abc5">code</Box> by clicking and clacking.
-        </Box>
-
-        <Box original={Link} code="abc6" address="https://github.com/assembleapp/hierarch" target="_blank" rel="noopener noreferrer" >
-          Learn Hierarch.
-        </Box>
-      </Box>
-
+    <Column>
       <Header>
-        <Scope
-          source="https://assemble-opposed.herokuapp.com/v1/graphql"
-          schema={{ companies: { '_': ['name', 'address' ]}}}
-          // anonymous
-          callback={(model, drop_clock) => {
-            if(model.companies.length >= 144) {
-              drop_clock('clock')
-              drop_clock('big_clock')
-              console.log(new Date().toISOString())
-            }
-          }}
-        >
-          {(model, _) => (
-            <>
-            <h3>See also:</h3>
-            <Board>
-              {model.companies.map((c, i) => (
-                <Link key={i} address={c.address} style={{fontSize: (6 + (c.danger || 5) * 2) + 'px'}}>{c.name}</Link>
-              ))}
-            </Board>
-            </>
-          )}
-        </Scope>
+        <Logo src={logo} alt="logo" />
+
+        <div>
+          <P>
+            Change <Code>code</Code> by clicking and clacking.
+          </P>
+
+          <Link address="https://github.com/assembleapp/hierarch" target="_blank" rel="noopener noreferrer" >
+            Learn Hierarch.
+          </Link>
+        </div>
       </Header>
-    </Box>
+
+      <Scope
+        source="https://assemble-opposed.herokuapp.com/v1/graphql"
+        schema={{ companies: { '_': ['name', 'address' ]}}}
+        // anonymous
+        callback={(model, drop_clock) => {
+          if(model.companies.length >= 36) {
+            drop_clock('clock')
+            drop_clock('big_clock')
+            console.log(new Date().toISOString())
+          }
+        }}
+      >
+        {(model, _) => (
+          <>
+          <h3>See also:</h3>
+          <Board>
+            {model.companies.map((c, i) => (
+              <Link key={i} address={c.address} style={{fontSize: (6 + (c.danger || 5) * 2) + 'px'}}>{c.name}</Link>
+            ))}
+          </Board>
+          </>
+        )}
+      </Scope>
+    </Column>
   );
 }
 
 const Board = styled.ul`
 display: grid;
-grid-template-columns: repeat(2, auto);
+grid-template-columns: repeat(3, auto);
 grid-row-gap: 0.2rem;
-grid-column-gap: 0.2rem;
+grid-column-gap: 0.6rem;
 `
 
 const LogoSpin = keyframes`
@@ -59,37 +59,42 @@ const LogoSpin = keyframes`
 // to { transform: rotate(360deg); }
 `
 
-const Layout = styled.div`
-text-align: center;
-display: grid;
-grid-template-columns: 1fr auto;
-grid-template-rows: 100vh;
+const Column = styled.div`
+// display: grid;
+// grid-template-columns: 1fr auto;
+// grid-template-rows: 100vh;
+min-height: 100vh;
 
 @media(max-width: 800px) {
   grid-template-columns: 1fr;
   grid-template-rows: 1fr auto;
   min-height: 100vh;
 }
-`
 
-const Logo = styled.img`
-height: 20vmin;
-
-@media (prefers-reduced-motion: no-preference) {
-  animation: ${LogoSpin} infinite 20s linear;
-}
-`
-
-const Header = styled.header`
 background-color: #8ab7ab;
 // min-height: 100vh;
 display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
-font-size: calc(10px + 2vmin);
 color: #365a92;
 padding: 0 4rem;
+`
+
+const Logo = styled.img`
+height: 100%;
+margin-right: 2rem;
+@media (prefers-reduced-motion: no-preference) {
+  animation: ${LogoSpin} infinite 20s linear;
+}
+`
+
+const Header = styled.header`
+display: flex;
+flex-direction: row;
+font-size: 1rem;
+height: 6rem;
+align-items: flex-end;
 `
 
 const Link = styled.a.attrs(p => ({
