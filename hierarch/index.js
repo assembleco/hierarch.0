02454d03112a4ed833d90049b707697c9edbed27
grace <grace.c.youngblood@gmail.com>
2021-01-21 18:15:13 -0500

    use `0.0.0.0:3000`

--- hierarch/index.js
+++ hierarch/index.js
@@ -14,7 +14,7 @@ responder.use(bodyParser.json())
 
 // https://enable-cors.org/server_expressjs.html
 responder.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:3000")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
 });
