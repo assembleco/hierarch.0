8eae4d734267bcbbe98f03fdac307e07830d7c10
grace <grace.c.youngblood@gmail.com>
2021-02-14 10:15:54 -0500

    [broken] begin browser-side hierarchy parsing.

--- src/hierarch/hierarchy.js
+++ src/hierarch/hierarchy.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { HierarchScope } from "./index"
 import styled from "styled-components"
+import Program from "./program"
 
 class Hierarchy extends React.Component {
     state = {
@@ -30,18 +31,7 @@ class Hierarchy extends React.Component {
       fetch(`http://0.0.0.0:4321/source?address=${this.state.address}`)
         .then(response => response.text())
         .then(response => this.setState({ source: response }))
-
-      fetch(`http://0.0.0.0:4321/hierarchy?address=${this.props.address}`, {
-        method: "GET",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-      })
-        .then(response => response.json())
-        .then(response => {
-          this.setState({ hierarchy: response })
-        })
+      this.parse_hierarchy()
     }
 
     render = () => (
@@ -49,6 +39,13 @@ class Hierarchy extends React.Component {
             {display_hierarchy_index(0, this.state.hierarchy)}
         </pre>
     )
+
+  parse_hierarchy = () => {
+    hierarchy(
+      this.state.source,
+      (h) => this.setState({ hierarchy: h })
+    )
+  }
 }
 
 const display_hierarchy_index = (index, hierarchy) => (
@@ -104,5 +101,125 @@ const Hierarchical = ({name, permissions, code}) => (
 const Border = styled.span`
     border: ${p => p.running ? "1px solid #ee00ee" : "none"};
 `
+
+const hierarchy = (source, callback) => {
+  var program = new Program(source)
+
+  var query = program.query(`
+        [(jsx_element) (jsx_self_closing_element)] @element
+        `)
+
+  var elements = query.map(m => {
+    if(m.captures.length !== 1) {
+      throw(
+        "oh no! our query has responded using a non-unique capture;\n" +
+        JSON.stringify(m.captures.map(c => program.display(c.node)))
+      )
+    }
+
+    var c = m.captures[0]
+    var name = null
+    var code = null
+    var permissions = []
+
+    if(c.node.type === "jsx_element") {
+      name = program.display(c.node.firstNamedChild.firstNamedChild)
+      if(name === "Box") {
+        // console.log()
+        // console.log(program.display(c.node))
+
+        code = program.display(c.node.namedChildren[0].namedChildren[2].namedChildren[1])
+        code = code.split('"').join('') // chop quotes.
+
+        name = program.display(c.node.namedChildren[0].namedChildren[1].namedChildren[1])
+        // console.log(name, code)
+        permissions = permissions.concat("g-4:change")
+      }
+      if(name === "Scope") {
+        permissions = permissions.concat("g-4:scope:grid")
+        var attrs = c.node.namedChildren[0].namedChildren.slice(1)
+        attrs = attrs.filter(a => (a.children[0] && a.children[0].type) === "property_identifier")
+        attrs = attrs.filter(a => ["source", "schema"].some(s => s === program.display(a.children[0])))
+        code = {}
+        attrs.forEach(a => code[program.display(a).split('=')[0]] = program.display(a).split('=').slice(1).join('='))
+        code.source = code.source.split('"').join('') // chop quotes.
+        code = JSON.stringify(code)
+        // console.log(code)
+      }
+    } else if (c.node.type === "jsx_self_closing_element") {
+      name = program.display(c.node.firstNamedChild)
+      if(name === "Box") {
+        // console.log()
+        // console.log(program.display(c.node))
+
+        code = program.display(c.node.namedChildren[2].namedChildren[1])
+        code = code.split('"').join('') // chop quotes.
+
+        name = program.display(c.node.namedChildren[1].namedChildren[1])
+        // console.log(name, code)
+        permissions = permissions.concat("g-4:resize")
+      }
+    } else {
+      throw (
+        "oh no! our query has responded on an undesired node;\n" +
+        c.node.toString() +
+        "\n---\n" +
+        program.display(c.node)
+      )
+    }
+
+    // console.log(program.display(c.node))
+    // console.log(permissions)
+
+    var appendages = {
+      jsx_self_closing_element: ["<", "/>"],
+      jsx_element: ["<", ">"],
+    }[c.node.type] || ["", ""]
+    name = appendages[0] + name + appendages[1]
+
+    return [
+      c.node.startIndex,
+      c.node.endIndex,
+      [],
+      name,
+      permissions,
+      code,
+    ]
+  })
+
+  var hierarchy = [0, program.source.length, [], "program", false]
+  var upper_chain = [hierarchy]
+  elements.forEach(e => {
+    var upper = upper_chain.slice(-1)[0]
+
+    if(e[0] < upper[0]) {
+      throw(
+        "oh no! our hierarchy is being processed out of order;\n" +
+        JSON.stringify(elements, null, 2) +
+        "\n---\n" +
+        JSON.stringify(hierarchy, null, 2)
+      )
+    }
+
+    while(e[1] > upper[1]) {
+      upper_chain = upper_chain.slice(0, upper_chain.length - 1)
+      upper = upper_chain.slice(-1)[0]
+    }
+
+    upper[2] = upper[2].concat([e])
+    // upper[4] = []
+    upper_chain = upper_chain.concat([e])
+  })
+
+  // const chop_arrays = (hierarchical) => {
+  //     hierarchical.shift()
+  //     hierarchical.shift()
+  //     if(hierarchical[0].length)
+  //         chop_arrays(hierarchical[0])
+  // }
+  // chop_arrays(hierarchy)
+
+  callback(hierarchy)
+}
 
 export default Hierarchy
---
+++ src/hierarch/program.js
@@ -0,0 +1,123 @@
+const Parser = require("tree-sitter")
+const JavaScript = require("tree-sitter-javascript")
+const Css = require("tree-sitter-css")
+
+const languages = {
+    js: JavaScript,
+    css: Css,
+}
+
+class Program {
+    constructor(name, source) {
+        this.name = name
+        this.source = source
+        this.parser = new Parser()
+        this.parser.setLanguage(JavaScript)
+        this.parsed = this.parser.parse(this.source)
+    }
+
+    display = (node) => this.parsed.getText(node)
+
+    replace_by_node(node, upgrade = "", options = {}) {
+        console.log("Replacing by node:")
+        console.log(this.display(node), " -> ", upgrade)
+
+        this.replace_by_indices(
+            node.startIndex + (options.beginningOffset || 0),
+            node.endIndex + (options.endingOffset || 0),
+            upgrade,
+        )
+    }
+
+    replace_by_indices(begin, end, upgrade = "") {
+        if(begin < 0) {
+            begin = str.length + begin
+            if(begin < 0) begin = 0
+        }
+
+        var changed_source = ''
+        var change = {}
+        ;([changed_source, change] = spliceInput(this.source, begin, end - begin, upgrade));
+        // this.source = this.source.slice(0, begin) + upgrade + this.source.slice(end)
+
+        this.parsed.edit(change)
+        this.source = changed_source
+    }
+
+    reparse() {
+        this.parsed = this.parser.parse(this.source, this.parsed)
+    }
+
+    parse_range_as_language(begin, end, lang) {
+        this.parser.setLanguage(languages[lang])
+        return this.parser.parse(this.source, null, {
+            includedRanges: [{
+                startIndex: begin,
+                endIndex: end,
+                startPosition: getExtent(this.source.slice(0, begin)),
+                endPosition: getExtent(this.source.slice(0, end)),
+            }]
+        })
+    }
+
+    use_language(lang) {
+        this.parser.setLanguage(languages[lang])
+    }
+
+    query(query, node = this.parsed.rootNode, lang = 'js') {
+        try {
+            if(query instanceof Array) {
+                return query.map(q => (
+                    new Parser.Query(languages[lang], q).matches(node)
+                )).flat(1)
+            }
+            return new Parser.Query(languages[lang], query).matches(node)
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    debug_query(query) {
+        var elements = query.map(m => {
+            return m.captures.map(c => {
+                return [
+                    c.name,
+                    // c.node.toString(),
+                    this.display(c.node),
+                ]
+            })
+        })
+        console.log(elements)
+    }
+}
+
+// source:
+// https://github.com/tree-sitter/node-tree-sitter/blob/a95bc5c723d0/test/tree_test.js#L327-L352
+function spliceInput(input, startIndex, lengthRemoved, newText) {
+    const oldEndIndex = startIndex + lengthRemoved;
+    const newEndIndex = startIndex + newText.length;
+    const startPosition = getExtent(input.slice(0, startIndex));
+    const oldEndPosition = getExtent(input.slice(0, oldEndIndex));
+    input = input.slice(0, startIndex) + newText + input.slice(oldEndIndex);
+    const newEndPosition = getExtent(input.slice(0, newEndIndex));
+    return [
+      input,
+      {
+        startIndex, startPosition,
+        oldEndIndex, oldEndPosition,
+        newEndIndex, newEndPosition
+      }
+    ];
+}
+
+function getExtent(text) {
+    let row = 0
+    let index;
+    for (index = 0; index != -1; index = text.indexOf('\n', index)) {
+      index++
+      row++;
+    }
+    return {row, column: text.length - index};
+}
+
+module.exports = Program
