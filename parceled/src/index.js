0426358f3a6ee2e1e0232714d166aa6554504bf8
grace <grace.c.youngblood@gmail.com>
2021-02-17 18:36:38 -0500

    [broken] parcel mangles `tree-sitter.wasm` bundle

--- parceled/src/index.js
+++ parceled/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+console.log('loaded')
+debugger
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App name="Jane" />, mountNode);
