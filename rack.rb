e344ae94e63c9c4400e314a9b5f36dee882c550c
grace <grace.c.youngblood@gmail.com>
2021-02-18 16:34:52 -0500

    Slim down rack app.

--- rack.rb
+++ rack.rb
@@ -1,34 +1,14 @@
 require "rubygems"
 require 'sinatra'
-require 'sinatra/cross_origin'
-
-configure do
-  enable :cross_origin
-end
-
-set :allow_origin, :any
-set :allow_methods, [:get, :post, :options]
 
 get '/tree-sitter.js' do
-  headers 'Access-Control-Allow-Origin' => 'http://0.0.0.0:3000'
-  response['Access-Control-Allow-Origin'] = 'http://0.0.0.0:3000'
-  File.read File.join("public", "tree-sitter.js")
+  send_file File.join("ts", "tree-sitter.js")
 end
 
 get '/tree-sitter.wasm' do
-  cross_origin
-
-  headers 'Access-Control-Allow-Origin' => 'http://0.0.0.0:3000'
-  response['Access-Control-Allow-Origin'] = 'http://0.0.0.0:3000'
-
-  headers 'Content-Type' => 'application/wasm;charset=utf-8'
-  response['Content-Type'] = 'application/wasm;charset=utf-8'
-
-  File.read File.join("public", "tree-sitter.wasm")
+  send_file File.join("ts", "tree-sitter.wasm")
 end
 
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-  200
+get "/" do
+  File.read("index.html")
 end
