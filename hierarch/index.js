935658accf76f2b09c62a28540f05f6b25f45cf8
grace <grace.c.youngblood@gmail.com>
2021-02-19 17:10:40 -0500

    [spacing] `hierarch/index` procedure

--- hierarch/index.js
+++ hierarch/index.js
@@ -15,9 +15,9 @@ responder.use(bodyParser.json())
 
 // https://enable-cors.org/server_expressjs.html
 responder.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:3000")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next();
+  res.header("Access-Control-Allow-Origin", "http://0.0.0.0:3000")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
 });
 
 responder.get("/source", (call, response) => {
