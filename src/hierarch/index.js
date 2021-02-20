88a2656609fad8651e7d004d709837dd7ce31082
grace <grace.c.youngblood@gmail.com>
2021-02-20 10:47:20 -0500

    [spacing] respace `src/hierarch/index`

--- src/hierarch/index.js
+++ src/hierarch/index.js
@@ -53,164 +53,164 @@ class Hierarch extends React.Component {
     this.pullSource()
   }
 
-    componentDidMount() {
-      if(!window.assemble || !window.assemble.repull) {
-        window.assemble = {}
-        window.assemble.repull = this.pullSource.bind(this)
-      }
+  componentDidMount() {
+    if(!window.assemble || !window.assemble.repull) {
+      window.assemble = {}
+      window.assemble.repull = this.pullSource.bind(this)
+    }
 
-        document.onkeydown = e => {
-            if(e.code === "Space") {
-                e.preventDefault()
-                var original_scroll = this.state.mouse.scroll
-                this.setState({ mouse: Object.assign(
-                    this.state.mouse,
-                    {
-                        hold: !this.state.mouse.hold,
-                        scroll: this.state.mouse.hold
-                        ? 0
-                        : window.pageYOffset,
-                    },
-                )})
-                if(!this.state.mouse.hold) {
-                    window.scrollBy(0, original_scroll)
-                }
-            }
+    document.onkeydown = e => {
+      if(e.code === "Space") {
+        e.preventDefault()
+        var original_scroll = this.state.mouse.scroll
+        this.setState({ mouse: Object.assign(
+          this.state.mouse,
+          {
+            hold: !this.state.mouse.hold,
+            scroll: this.state.mouse.hold
+            ? 0
+            : window.pageYOffset,
+          },
+        )})
+        if(!this.state.mouse.hold) {
+          window.scrollBy(0, original_scroll)
         }
+      }
     }
+  }
 
-    componentWillUnmount() {
-      document.onkeydown = null
+  componentWillUnmount() {
+    document.onkeydown = null
 
-      window.assemble.repull = null
-      if(!Object.keys(window.assemble).length) window.assemble = null
-    }
+    window.assemble.repull = null
+    if(!Object.keys(window.assemble).length) window.assemble = null
+  }
 
-    componentDidUpdate() {
-        document.oncontextmenu = this.state.open
-            ? this.secondaryClick
+  componentDidUpdate() {
+    document.oncontextmenu = this.state.open
+      ? this.secondaryClick
+      : null
+  }
+
+  secondaryClick = (e) => {
+    e.preventDefault()
+    apply_boxes(this.state.index, this.state.address)
+      // .then(response => response.text())
+      // .then(response => makeProgram(response))
+      // .then(program => this.setState({ index: program }))
+      .then(this.pullSource)
+  }
+
+  signal = (signal, code) => {
+    console.log("Signal", signal, code)
+    this.setState({ scope: { code, signal } })
+  }
+
+  render = () => (
+    <HierarchScope.Provider
+      value={{
+        address: this.state.address,
+        index: this.state.index,
+        open: this.state.open,
+        chosen: this.state.scope,
+        signal: this.signal,
+      }}
+    >
+      <Display
+        hold={this.state.mouse.hold}
+        scroll={this.state.mouse.scroll}
+        open={this.state.open}
+        onMouseMove={(e) => {
+          if(this.state.open && !this.state.mouse.hold)
+            this.setState({ mouse: { x: e.clientX, y: e.clientY }})
+
+          var code_key = e.target
+            && typeof e.target.getAttribute === 'function'
+            ? e.target.getAttribute("data-code")
             : null
-    }
 
-    secondaryClick = (e) => {
-      e.preventDefault()
-      apply_boxes(this.state.index, this.state.address)
-        // .then(response => response.text())
-        // .then(response => makeProgram(response))
-        // .then(program => this.setState({ index: program }))
-        .then(this.pullSource)
-    }
+          if(code_key && this.state.scope.signal === "display")
+            this.signal("display", code_key)
+        }}
+      >
+        {this.props.children}
 
-    signal = (signal, code) => {
-        console.log("Signal", signal, code)
-        this.setState({ scope: { code, signal } })
-    }
+        {this.state.open
+        ?
+          <Sidebar
+            close={() => this.setState({ open: false })}
+            display={(code) => this.signal("display", code)}
+            place={this.state.mouse}
+          >
+            {this.state.scope.signal === 'grid'
+            ?
+              <Scope
+              {...JSON.parse(this.state.scope.code)}
+              >
+                {(model, upgrade) => (
+                  <Grid
+                  schema={JSON.parse(this.state.scope.code).schema}
+                  model={model}
+                  upgradeRecord={this.upgradeRecord(model, upgrade)}
+                  />
+                )}
+              </Scope>
+            : (
+              this.state.scope.signal === 'change'
+              ?
+                <Change.Board>
+                  <Change.Color name="background-color" />
+                  <Change.Color name="color" />
+                  <Change.Size name="font-size" />
+                  <Change.Size name="width" />
+                  <Change.Size name="height" />
+                </Change.Board>
+              :
+                <Hierarchy
+                  hierarchy={this.state.hierarchy}
+                  display={this.props.display}
+                />
+              )
+            }
+          </Sidebar>
+        :
+          <Corner>
+            <Logo
+              size={20}
+              repeat={100000}
+              onClick={() => this.setState({ open: true, mouse: { hold: false } })}
+            />
+          </Corner>
+        }
+      </Display>
+    </HierarchScope.Provider>
+  )
 
-    render = () => (
-        <HierarchScope.Provider
-          value={{
-            address: this.state.address,
-            index: this.state.index,
-            open: this.state.open,
-            chosen: this.state.scope,
-            signal: this.signal,
-          }}
-        >
-            <Display
-                hold={this.state.mouse.hold}
-                scroll={this.state.mouse.scroll}
-                open={this.state.open}
-                onMouseMove={(e) => {
-                    if(this.state.open && !this.state.mouse.hold)
-                        this.setState({ mouse: { x: e.clientX, y: e.clientY }})
-
-                    var code_key = e.target
-                        && typeof e.target.getAttribute === 'function'
-                        ? e.target.getAttribute("data-code")
-                        : null
-
-                    if(code_key && this.state.scope.signal === "display")
-                        this.signal("display", code_key)
-                }}
-            >
-                {this.props.children}
-
-                {this.state.open
-                ?
-                    <Sidebar
-                        close={() => this.setState({ open: false })}
-                        display={(code) => this.signal("display", code)}
-                        place={this.state.mouse}
-                    >
-                        {this.state.scope.signal === 'grid'
-                        ?
-                            <Scope
-                            {...JSON.parse(this.state.scope.code)}
-                            >
-                                {(model, upgrade) => (
-                                    <Grid
-                                    schema={JSON.parse(this.state.scope.code).schema}
-                                    model={model}
-                                    upgradeRecord={this.upgradeRecord(model, upgrade)}
-                                    />
-                                )}
-                            </Scope>
-                        : (
-                            this.state.scope.signal === 'change'
-                            ?
-                                <Change.Board>
-                                    <Change.Color name="background-color" />
-                                    <Change.Color name="color" />
-                                    <Change.Size name="font-size" />
-                                    <Change.Size name="width" />
-                                    <Change.Size name="height" />
-                                </Change.Board>
-                            :
-                                <Hierarchy
-                                    hierarchy={this.state.hierarchy}
-                                    display={this.props.display}
-                                />
-                            )
-                        }
-                    </Sidebar>
-                :
-                    <Corner>
-                        <Logo
-                            size={20}
-                            repeat={100000}
-                            onClick={() => this.setState({ open: true, mouse: { hold: false } })}
-                        />
-                    </Corner>
-                }
-            </Display>
-        </HierarchScope.Provider>
+  upgradeRecord = (model, upgrade) => (rowIndex, columnId, value) => {
+    var company = model.companies.toJSON()[rowIndex]
+    console.log(company)
+    var grade = Object.assign(
+      {},
+      {
+        name: company.name,
+        address: company.address,
+        number: company.number,
+      },
+      { [columnId]: value },
     )
 
-    upgradeRecord = (model, upgrade) => (rowIndex, columnId, value) => {
-        var company = model.companies.toJSON()[rowIndex]
-        console.log(company)
-        var grade = Object.assign(
-            {},
-            {
-                name: company.name,
-                address: company.address,
-                number: company.number,
-            },
-            { [columnId]: value },
-        )
-
-        upgrade(grade)
-    }
+    upgrade(grade)
+  }
 }
 
 const Display = styled.div`
 margin: 0;
 ${({hold, scroll, open}) => open && hold && `
-position: fixed;
-top: ${-1 * (scroll || 0)}px;
-left: 0;
-right: 0;
-bottom: 0;
+  position: fixed;
+  top: ${-1 * (scroll || 0)}px;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `}
 `
 
