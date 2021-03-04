32cc7b7a708b9e4fa0ff3557e2682ba8451b850e
grace <grace.c.youngblood@gmail.com>
2021-03-04 13:30:05 -0500

    Change hierarch channel on a whim.

--- hierarch.js
+++ hierarch.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const reload = require("express-reload")
-const port = 4321
+const channel = parseInt(process.env.HIERARCH_CHANNEL, 10) || 4321
 
 const responder = express()
 const hierarchy = __dirname + "/hierarch/"
 
 responder.use(reload(hierarchy))
 
-responder.listen(port, () => {
-    console.log(`Hierarch up on ${port}.`)
+responder.listen(channel, () => {
+    console.log(`Hierarch up on ${channel}.`)
 })
