1df5519ae5037f4442bf788e1ffc312772e51560
grace <grace.c.youngblood@gmail.com>
2021-04-22 18:30:19 -0400

    Add basic layout of size form.

--- src/hierarch/menu/size.js
+++ src/hierarch/menu/size.js
@@ -1,9 +1,34 @@
 import React from "react"
+import styled from "styled-components"
 
 var Size = () => (
-  <div>
-  Size
-  </div>
+  <Grid>
+    <span>Width</span>
+    <input type="text" placeholder="None" />
+
+    <span>Height</span>
+    <input type="text" placeholder="None" />
+
+    <span>Min W</span>
+    <input type="text" placeholder="None" />
+
+    <span>Min H</span>
+    <input type="text" placeholder="None" />
+
+    <span>Max W</span>
+    <input type="text" placeholder="None" />
+
+    <span>Max H</span>
+    <input type="text" placeholder="None" />
+
+    <span>Overflow</span>
+  </Grid>
 )
+
+var Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(4, 1fr);
+grid-template-rows: repeat(3, 1fr) 1.5fr;
+`
 
 export default Size
