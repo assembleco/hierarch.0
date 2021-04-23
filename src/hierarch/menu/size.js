c7e0e62526829806e0d72c50c2de62cdf7a0ad02
grace <grace.c.youngblood@gmail.com>
2021-04-22 20:22:25 -0400

    Hook up scope harness around the side `width` box.

--- src/hierarch/menu/size.js
+++ src/hierarch/menu/size.js
@@ -1,28 +1,47 @@
 import React from "react"
 import styled from "styled-components"
+import { Observer } from "mobx-react"
+import { observable, autorun } from "mobx"
+
+import { HierarchScope } from "../index"
+
+var width = observable.box(0)
+autorun(() => console.log("width", width.get()))
 
 var Size = () => (
-  <Grid>
-    <span>Width</span>
-    <input type="text" placeholder="None" />
+  <HierarchScope.Consumer>{scope => (
+    <Observer>{() => (
+      <Grid>
+        <span>Width</span>
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => {
+            console.log(scope.chosen)
+            width.set(e.target.value)
+          }}
+          value={width.get()}
+        />
 
-    <span>Height</span>
-    <input type="text" placeholder="None" />
+        <span>Height</span>
+        <input type="text" placeholder="None" />
 
-    <span>Min W</span>
-    <input type="text" placeholder="None" />
+        <span>Min W</span>
+        <input type="text" placeholder="None" />
 
-    <span>Min H</span>
-    <input type="text" placeholder="None" />
+        <span>Min H</span>
+        <input type="text" placeholder="None" />
 
-    <span>Max W</span>
-    <input type="text" placeholder="None" />
+        <span>Max W</span>
+        <input type="text" placeholder="None" />
 
-    <span>Max H</span>
-    <input type="text" placeholder="None" />
+        <span>Max H</span>
+        <input type="text" placeholder="None" />
 
-    <span>Overflow</span>
-  </Grid>
+        <span>Overflow</span>
+      </Grid>
+    )}</Observer>
+  )}</HierarchScope.Consumer>
 )
 
 var Grid = styled.div`
