1c3f0778c0fc41940ec306a65c07600ab283b2f7
grace <grace.c.youngblood@gmail.com>
2021-04-23 12:56:43 -0400

    Hook up all remaining size menu boxes.

--- src/hierarch/menu/size.js
+++ src/hierarch/menu/size.js
@@ -14,23 +14,48 @@ var Size = () => (
           type="text"
           placeholder="None"
           onChange={(e) => scope.changes['width'] = e.target.value}
-          value={scope.changes.width || ''}
+          value={scope.changes['width'] || ''}
         />
 
         <span>Height</span>
-        <input type="text" placeholder="None" />
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => scope.changes['height'] = e.target.value}
+          value={scope.changes['height'] || ''}
+        />
 
         <span>Min W</span>
-        <input type="text" placeholder="None" />
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => scope.changes['min-width'] = e.target.value}
+          value={scope.changes['min-width'] || ''}
+        />
 
         <span>Min H</span>
-        <input type="text" placeholder="None" />
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => scope.changes['min-height'] = e.target.value}
+          value={scope.changes['min-height'] || ''}
+        />
 
         <span>Max W</span>
-        <input type="text" placeholder="None" />
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => scope.changes['max-width'] = e.target.value}
+          value={scope.changes['max-width'] || ''}
+        />
 
         <span>Max H</span>
-        <input type="text" placeholder="None" />
+        <input
+          type="text"
+          placeholder="None"
+          onChange={(e) => scope.changes['max-height'] = e.target.value}
+          value={scope.changes['max-height'] || ''}
+        />
 
         <span>Overflow</span>
       </Grid>
