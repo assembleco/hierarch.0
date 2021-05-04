18629e9dcecbd49e2c9ab62a78b6f03205d84058
grace <grace.c.youngblood@gmail.com>
2021-05-04 12:39:47 -0400

    [code spacing] realign `./display/hierarchy.js`

--- src/hierarch/display/hierarchy.js
+++ src/hierarch/display/hierarchy.js
@@ -16,46 +16,46 @@ class Hierarchy extends React.Component {
 }
 
 const display_hierarchy_index = (index, hierarchy) => (
-    hierarchy[2].map(h => (
-        <>
-        {("  ".repeat(index))}
-        <Hierarchical name={h[3]} permissions={h[4]} code={h[5]} />
-        {"\n"}
-        {display_hierarchy_index(index + 1, h)}
-        </>
-    ))
+  hierarchy[2].map(h => (
+    <>
+    {("  ".repeat(index))}
+    <Hierarchical name={h[3]} permissions={h[4]} code={h[5]} />
+    {"\n"}
+    {display_hierarchy_index(index + 1, h)}
+    </>
+  ))
 )
 
 const Hierarchical = observer(({name, permissions, code}) => (
-    <HierarchScope.Consumer>
-    {scope => (
-      <Observer>{() => (
-        <Border running={scope.display === code}>
+  <HierarchScope.Consumer>
+  {scope => (
+    <Observer>{() => (
+      <Border running={scope.display === code}>
 
-        {
-        permissions.indexOf("g-4:change") !== -1
-        ?
-        <a
-            key={code}
-            href="#"
-            onClick={() => scope.change = code}
-            onMouseOver={() => scope.display = code}
-        >{name}</a>
-        :
-        <span
-            key={code}
-            onMouseOver={() => scope.display = code}
-        >{name}</span>
-        }
+      {
+      permissions.indexOf("g-4:change") !== -1
+      ?
+      <a
+        key={code}
+        href="#"
+        onClick={() => scope.change = code}
+        onMouseOver={() => scope.display = code}
+      >{name}</a>
+      :
+      <span
+        key={code}
+        onMouseOver={() => scope.display = code}
+      >{name}</span>
+      }
 
-        </Border>
-      )}</Observer>
-    )}
-    </HierarchScope.Consumer>
+      </Border>
+    )}</Observer>
+  )}
+  </HierarchScope.Consumer>
 ))
 
 const Border = styled.span`
-    border: ${p => p.running ? "1px solid #ee00ee" : "none"};
+border: ${p => p.running ? "1px solid #ee00ee" : "none"};
 `
 
 export default Hierarchy
