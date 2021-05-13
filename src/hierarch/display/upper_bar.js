6a0e286a818003cb58c60b05033e28aafbe05468
grace <grace.c.youngblood@gmail.com>
2021-05-13 17:39:15 -0400

    begin applying modal

--- src/hierarch/display/upper_bar.js
+++ src/hierarch/display/upper_bar.js
@@ -2,11 +2,37 @@ import React from "react"
 import styled from "styled-components"
 import { Observer } from "mobx-react"
 
+import { Portal } from "reakit/Portal";
+import {
+  useDialogState,
+  Dialog,
+  DialogDisclosure,
+  DialogBackdrop,
+} from "reakit/Dialog";
+
+function Modal() {
+  const dialog = useDialogState();
+  return (
+    <>
+      <ModalCollapsed {...dialog}>Open dialog</ModalCollapsed>
+      <DialogBackdrop {...dialog}>
+        <ModalOpened {...dialog} aria-label="Welcome">
+          Welcome to Reakit!
+        </ModalOpened>
+      </DialogBackdrop>
+    </>
+  );
+}
+
 class UpperBar extends React.Component {
   render = () => (
     <Observer>{() => (
       <Bar>
         {this.props.children}
+
+        <UpperBar.Header>
+          <Modal/>
+        </UpperBar.Header>
       </Bar>
     )}</Observer>
   )
@@ -22,8 +48,27 @@ background-color: #faf9dd;
 border-bottom: 2px solid #3d3b11;
 display: flex;
 flex-direction: row;
-justify-content: space-between;
-align-items: baseline;
+justify-content: flex-start;
+align-items: flex-start;
+`
+
+UpperBar.Header = styled.div`
+margin-left: 10rem;
+`
+
+var ModalCollapsed = styled(DialogDisclosure)`
+height: 2rem;
+width: 60vw;
+background-color: #a0a0d0;
+border: 2px solid #3d3b11;
+border-radius: 4px;
+margin-top: 1rem;
+`
+
+var ModalOpened = styled(Dialog)`
+z-index: 10;
+margin-top: 12rem;
+margin-left: 12rem;
 `
 
 export default UpperBar
