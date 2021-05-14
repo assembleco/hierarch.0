fc9f407c607855d8a612ea6f04957273d79b2a85
grace <grace.c.youngblood@gmail.com>
2021-05-14 12:50:30 -0400

    Make a modal box workaround.

--- src/hierarch/display/upper_bar.js
+++ src/hierarch/display/upper_bar.js
@@ -2,36 +2,25 @@ import React from "react"
 import styled from "styled-components"
 import { Observer } from "mobx-react"
 
-import { Portal } from "reakit/Portal";
-import {
-  useDialogState,
-  Dialog,
-  DialogDisclosure,
-  DialogBackdrop,
-} from "reakit/Dialog";
-
-function Modal() {
-  const dialog = useDialogState();
-  return (
-    <>
-      <ModalCollapsed {...dialog}>Open dialog</ModalCollapsed>
-      <DialogBackdrop {...dialog}>
-        <ModalOpened {...dialog} aria-label="Welcome">
-          Welcome to Reakit!
-        </ModalOpened>
-      </DialogBackdrop>
-    </>
-  );
-}
-
 class UpperBar extends React.Component {
+  state = {
+    open: false,
+  }
+
   render = () => (
     <Observer>{() => (
       <Bar>
         {this.props.children}
 
         <UpperBar.Header>
-          <Modal/>
+          <Modal
+            open={this.state.open}
+            onClick={() => this.setState({open: !this.state.open})}
+          >
+            {this.state.open &&
+              <Monaco/>
+            }
+          </Modal>
         </UpperBar.Header>
       </Bar>
     )}</Observer>
@@ -41,6 +30,7 @@ class UpperBar extends React.Component {
 var Bar = styled.div`
 height: 4rem;
 position: fixed;
+overflow: visible;
 top: 0;
 left: 0;
 right: 0;
@@ -56,19 +46,21 @@ UpperBar.Header = styled.div`
 margin-left: 10rem;
 `
 
-var ModalCollapsed = styled(DialogDisclosure)`
-height: 2rem;
-width: 60vw;
+var Modal = styled.div`
+height: ${({open}) => open ? '80vh' : '0rem'};
+transition: height ease-in-out 0.5s;
+width: calc(100vw - 12rem - 18rem - 2rem);
 background-color: #a0a0d0;
 border: 2px solid #3d3b11;
 border-radius: 4px;
 margin-top: 1rem;
+padding: 1rem;
 `
 
-var ModalOpened = styled(Dialog)`
-z-index: 10;
-margin-top: 12rem;
-margin-left: 12rem;
+var Monaco = styled.div`
+border: 2px solid #3d3b11;
+background: #d0d0d0;
+height: 100%;
 `
 
 export default UpperBar
