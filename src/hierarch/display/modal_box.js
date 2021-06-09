e8fcef1888b0051b7ad7e89a2a5d5003866e8b6d
grace <grace.c.youngblood@gmail.com>
2021-06-09 15:30:38 -0400

    Realign modal box.

--- src/hierarch/display/modal_box.js
+++ src/hierarch/display/modal_box.js
@@ -1,14 +1,14 @@
 import styled from "styled-components"
 
 import { Icon, InlineIcon } from '@iconify/react';
-import insertBeforeR from '@iconify-icons/gg/insert-before-r';
-import insertAfterR from '@iconify-icons/gg/insert-after-r';
+import add_behind from '@iconify-icons/gg/insert-before-r';
+import add_ahead from '@iconify-icons/gg/insert-after-r';
 
 
 var ModalBox = () => (
   <Border>
-    <Icon icon={insertBeforeR} height="2rem" color="#3d3b11" />
-    <Icon icon={insertAfterR} height="2rem" color="#3d3b11" />
+    <Icon icon={add_ahead} height="2rem" color="#3d3b11" />
+    <Icon icon={add_behind} height="2rem" color="#3d3b11" />
   </Border>
 )
 
@@ -19,6 +19,9 @@ background-color: #faf9dd;
 padding: 0.5rem;
 margin-left: 2.4rem;
 height: 2rem;
+display: grid;
+grid-template-columns: repeat(2, 2rem);
+grid-column-gap: 1rem;
 `
 
 export default ModalBox
