6c6d3ab24676b30cb27860df806a577183af37d0
grace <grace.c.youngblood@gmail.com>
2021-06-09 15:27:46 -0400

    Add a couple icons in modal box.

--- src/hierarch/display/modal_box.js
+++ src/hierarch/display/modal_box.js
@@ -1,8 +1,14 @@
 import styled from "styled-components"
 
+import { Icon, InlineIcon } from '@iconify/react';
+import insertBeforeR from '@iconify-icons/gg/insert-before-r';
+import insertAfterR from '@iconify-icons/gg/insert-after-r';
+
+
 var ModalBox = () => (
   <Border>
-  Hello.
+    <Icon icon={insertBeforeR} height="2rem" color="#3d3b11" />
+    <Icon icon={insertAfterR} height="2rem" color="#3d3b11" />
   </Border>
 )
 
@@ -12,6 +18,7 @@ border-radius: 4px;
 background-color: #faf9dd;
 padding: 0.5rem;
 margin-left: 2.4rem;
+height: 2rem;
 `
 
 export default ModalBox
