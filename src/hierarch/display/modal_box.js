aec68aeeb78b7c278ad7f17ab3bcb2bdf03c9b07
grace <grace.c.youngblood@gmail.com>
2021-06-09 16:48:40 -0400

    Add a `borderOutside` icon to modal box.

--- src/hierarch/display/modal_box.js
+++ src/hierarch/display/modal_box.js
@@ -4,12 +4,14 @@ import { observer } from "mobx-react"
 import { Icon, InlineIcon } from '@iconify/react';
 import add_behind from '@iconify-icons/gg/insert-before-r';
 import add_ahead from '@iconify-icons/gg/insert-after-r';
+import borderOutside from '@iconify-icons/mdi/border-outside';
+import borderInside from '@iconify-icons/mdi/border-inside';
 
 var ModalBox = ({ scope }) => (
   <Border>
     <Icon
       icon={add_ahead}
-      height="2rem"
+      width="2rem"
       color={scope.signal === "add_ahead" ? "blue" : "#3d3b11"}
       onClick={() => {
         scope.signal === "add_ahead"
@@ -20,13 +22,21 @@ var ModalBox = ({ scope }) => (
 
     <Icon
       icon={add_behind}
-      height="2rem"
-      color="#3d3b11"
+      width="2rem"
       color={scope.signal === "add_behind" ? "blue" : "#3d3b11"}
       onClick={() => {
         scope.signal === "add_behind"
         ? scope.sign(null)
         : scope.sign("add_behind")
+      }}
+    />
+
+    <Icon
+      icon={borderOutside}
+      width="2rem"
+      color="#3d3b11"
+      onClick={() => {
+        debugger
       }}
     />
   </Border>
@@ -36,11 +46,11 @@ const Border = styled.div`
 border: 2px solid #3d3b11;
 border-radius: 4px;
 background-color: #faf9dd;
-padding: 0.5rem;
+padding: 0.5rem 1rem;
 margin-left: 2.4rem;
 height: 2rem;
 display: grid;
-grid-template-columns: repeat(2, 2rem);
+grid-template-columns: repeat(3, 2rem);
 grid-column-gap: 1rem;
 `
 
